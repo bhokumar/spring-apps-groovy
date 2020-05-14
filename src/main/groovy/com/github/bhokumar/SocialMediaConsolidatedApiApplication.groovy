5d405ab14e2c065e366f2a88400921e406c8fbe7
bhokumar <bhoopendra.akgec@gmail.com>
2020-05-15 01:42:29 +0530

    Adding Social media application

---
+++ src/main/groovy/com/github/bhokumar/SocialMediaConsolidatedApiApplication.groovy
@@ -0,0 +1,13 @@
+package com.github.bhokumar
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SocialMediaConsolidatedApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SocialMediaConsolidatedApiApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/github/bhokumar/SocialMediaConsolidatedApiApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.github.bhokumar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialMediaConsolidatedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
